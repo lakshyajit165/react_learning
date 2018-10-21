988fdfd3d78a3a68628b813c7a52b89e7a7baae0
lakshyajit165 <lakshyajit165@gmail.com>
2018-10-21 13:08:32 +0530

    Functions as props

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -12,13 +12,23 @@ class App extends Component {
       ]
   }
 
+  addNinja = (ninja) =>{
+
+    ninja.id = Math.random();
+    let ninjas = [...this.state.ninjas, ninja]
+
+    this.setState({
+       ninjas: ninjas
+    })
+  }
+
   render() {
     return (
       <div className="App">
         <h1>My First React App</h1>
         <p>Welcome :-)</p>
         <Ninjas ninjas={this.state.ninjas}/>
-        <AddNinja />
+        <AddNinja addNinja = {this.addNinja}/>
       </div>
     );
   }
